b4e2fcc2778a9f88faf83955d1a0739477f87960
otterfamily <zackmarlow@gmail.com>
2024-07-18 16:03:05 -0700

    move mouse physics to fixedupdate

--- Assets/SimpleMouse.cs
+++ Assets/SimpleMouse.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class SimpleMouse : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+
     //dragged object
     private GameObject draggedObject;
     //offset relative to the grabbed object, used for establishing pivot point for drag physics
@@ -33,32 +33,42 @@ public class SimpleMouse : MonoBehaviour
             {
                 Debug.Log("Hit object: " + hit.collider.gameObject.name);
                 Rigidbody2D rb = hit.collider.GetComponent<Rigidbody2D>();
-                if(rb == null)
+                if (rb == null)
                 {
                     Debug.Log("No Rigidbody2D found on object");
                     return;
                 }
 
-                if(rb.bodyType == RigidbodyType2D.Dynamic)
+                if (rb.bodyType == RigidbodyType2D.Dynamic)
                 {
                     Debug.Log("Kinematic Rigidbody2D found on object");
                     Grab(hit);
-                }else {
-                    if(hit.collider.GetComponent<ClickEventEmitter>() != null){
+                }
+                else
+                {
+                    if (hit.collider.GetComponent<ClickEventEmitter>() != null)
+                    {
                         Debug.Log("ClickEventEmitter found on object");
                         hit.collider.GetComponent<ClickEventEmitter>().Click();
-                    }else{
+                    }
+                    else
+                    {
                         Debug.Log("No ClickEventEmitter found on object");
-                    
+
                     }
                 }
             }
         }
-        if(Input.GetMouseButtonUp(0)){
+        if (Input.GetMouseButtonUp(0))
+        {
             DropHeld();
         }
+    }
+    void FixedUpdate()
+    {
 
-        if(draggedObject != null){
+        if (draggedObject != null)
+        {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
             Vector2 attractionVector = mousePos2D - (Vector2)draggedObject.transform.position;
@@ -66,13 +76,16 @@ public class SimpleMouse : MonoBehaviour
             // draggedObject.transform.position = mousePos2D - (Vector2)draggedObject.transform.TransformVector(localOffset);
         }
     }
-    void Grab(RaycastHit2D hit){
+    void Grab(RaycastHit2D hit)
+    {
         draggedObject = hit.collider.gameObject;
         // draggedObject.GetComponent<Rigidbody2D>().simulated = false;
         localOffset = draggedObject.transform.InverseTransformPoint(hit.point);
     }
-    void DropHeld(){
-        if(draggedObject != null){
+    void DropHeld()
+    {
+        if (draggedObject != null)
+        {
             // draggedObject.GetComponent<Rigidbody2D>().simulated = true;
             //zero out forces
             // draggedObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
